dd187ee1cabfefc8adbd164e8837c450c3a735ce
Maxim Tarala <m.tarala@premiumitsolution.com>
2021-06-30 20:37:45 +0300

    initial commit

---
+++ src/main/java/ru/tarala/characterapi/CharacterApiApp.java
@@ -0,0 +1,12 @@
+package ru.tarala.characterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharacterApiApp {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CharacterApiApp.class, args);
+  }
+}
